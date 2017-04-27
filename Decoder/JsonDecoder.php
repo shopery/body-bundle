86199fdba3464945793165bf876c292b8ae9f1f9
Roger Gros <regor1985@gmail.com>
2017-04-27 16:23:27 +0200

    Moved depth to constant

--- Decoder/JsonDecoder.php
+++ Decoder/JsonDecoder.php
@@ -4,8 +4,10 @@ namespace Shopery\Bundle\BodyBundle\Decoder;
 
 class JsonDecoder implements Decoder
 {
+    const DECODE_DEPTH = 24;
+
     public function decode($content)
     {
-        return json_decode($content, true, 24);
+        return json_decode($content, true, self::DECODE_DEPTH);
     }
 }
