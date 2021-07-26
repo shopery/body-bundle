7ec4d2984b7d47ee457255c7a8259045650b5f37
David Castro <david.castro@shopery.com>
2021-07-26 09:48:39 +0200

    Add helper functions to get typed body params

--- Request/Body.php
+++ Request/Body.php
@@ -8,13 +8,13 @@
 
 namespace Shopery\Bundle\BodyBundle\Request;
 
-use Traversable;
-use Iterator;
-use DomainException;
 use ArrayIterator;
-
+use DomainException;
+use Iterator;
 use Symfony\Component\DependencyInjection\Exception\BadMethodCallException;
 use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
+use Traversable;
+use UnexpectedValueException;
 
 class Body implements \ArrayAccess, \IteratorAggregate
 {
@@ -23,10 +23,12 @@ class Body implements \ArrayAccess, \IteratorAggregate
     public function __construct($content)
     {
         if (!$this->isValidContent($content)) {
-            throw new DomainException(sprintf(
-                'Invalid body content: %s',
-                serialize($content)
-            ));
+            throw new DomainException(
+                sprintf(
+                    'Invalid body content: %s',
+                    serialize($content)
+                )
+            );
         }
 
         $this->content = $content;
@@ -49,10 +51,12 @@ class Body implements \ArrayAccess, \IteratorAggregate
     public function offsetGet($offset)
     {
         if (!$this->offsetExists($offset)) {
-            throw new BadRequestHttpException(sprintf(
-                'Looking for "%s" attribute in body, not found',
-                $offset
-            ));
+            throw new BadRequestHttpException(
+                sprintf(
+                    'Looking for "%s" attribute in body, not found',
+                    $offset
+                )
+            );
         }
 
         return $this->content[$offset];
@@ -60,18 +64,22 @@ class Body implements \ArrayAccess, \IteratorAggregate
 
     public function offsetSet($offset, $value)
     {
-        throw new BadMethodCallException(sprintf(
-            'Unexpected call to "%s", body is read-only.',
-            __METHOD__
-        ));
+        throw new BadMethodCallException(
+            sprintf(
+                'Unexpected call to "%s", body is read-only.',
+                __METHOD__
+            )
+        );
     }
 
     public function offsetUnset($offset)
     {
-        throw new BadMethodCallException(sprintf(
-            'Unexpected call to "%s", body is read-only.',
-            __METHOD__
-        ));
+        throw new BadMethodCallException(
+            sprintf(
+                'Unexpected call to "%s", body is read-only.',
+                __METHOD__
+            )
+        );
     }
 
     public function getIterator()
@@ -81,6 +89,137 @@ class Body implements \ArrayAccess, \IteratorAggregate
         }
 
         return $this->content;
+    }
+
+    /***********************************************************
+     *                                                         *
+     *                     HELPER METHODS                      *
+     *                                                         *
+     ***********************************************************/
+
+    public function string(string $param): string
+    {
+        $paramValue = $this->nullableString($param);
+
+        if (null === $paramValue) {
+            throw new UnexpectedValueException("Requested string value not found in request param <$param>");
+        }
+
+        return $paramValue;
+    }
+
+    public function nullableString(string $param, ?string $defaultValue = null): ?string
+    {
+        $paramValue = $this->get($param);
+
+        if (null !== $paramValue && !is_string($paramValue)) {
+            throw new UnexpectedValueException(
+                sprintf('Expected string in request param <%s>, but found %s', $param, gettype($paramValue))
+            );
+        }
+
+        return $paramValue ?? $defaultValue;
+    }
+
+    public function int(int $param): int
+    {
+        $paramValue = $this->nullableInt($param);
+
+        if (null === $paramValue) {
+            throw new UnexpectedValueException("Requested int value not found in request param <$param>");
+        }
+
+        return $paramValue;
+    }
+
+    public function nullableInt(int $param, ?int $defaultValue = null): ?int
+    {
+        $paramValue = $this->get($param);
+
+        if (null !== $paramValue && !is_int($paramValue)) {
+            throw new UnexpectedValueException(
+                sprintf('Expected int in request param <%s>, but found %s', $param, gettype($paramValue))
+            );
+        }
+
+        return $paramValue ?? $defaultValue;
+    }
+
+    public function float(float $param): float
+    {
+        $paramValue = $this->nullableFloat($param);
+
+        if (null === $paramValue) {
+            throw new UnexpectedValueException("Requested float value not found in request param <$param>");
+        }
+
+        return $paramValue;
+    }
+
+    public function nullableFloat(float $param, ?float $defaultValue = null): ?float
+    {
+        $paramValue = $this->get($param);
+
+        if (null !== $paramValue && !is_float($paramValue)) {
+            throw new UnexpectedValueException(
+                sprintf('Expected float in request param <%s>, but found %s', $param, gettype($paramValue))
+            );
+        }
+
+        return $paramValue ?? $defaultValue;
+    }
+
+    public function bool(bool $param): bool
+    {
+        $paramValue = $this->nullableBool($param);
+
+        if (null === $paramValue) {
+            throw new UnexpectedValueException("Requested bool value not found in request param <$param>");
+        }
+
+        return $paramValue;
+    }
+
+    public function nullableBool(bool $param, ?bool $defaultValue = null): ?bool
+    {
+        $paramValue = $this->get($param);
+
+        if (null !== $paramValue && !is_bool($paramValue)) {
+            throw new UnexpectedValueException(
+                sprintf('Expected bool in request param <%s>, but found %s', $param, gettype($paramValue))
+            );
+        }
+
+        return $paramValue ?? $defaultValue;
+    }
+
+    public function array(array $param): array
+    {
+        $paramValue = $this->nullableArray($param);
+
+        if (null === $paramValue) {
+            throw new UnexpectedValueException("Requested array value not found in request param <$param>");
+        }
+
+        return $paramValue;
+    }
+
+    public function nullableArray(array $param, ?array $defaultValue = null): ?array
+    {
+        $paramValue = $this->get($param);
+
+        if (null !== $paramValue && !is_array($paramValue)) {
+            throw new UnexpectedValueException(
+                sprintf('Expected array in request param <%s>, but found %s', $param, gettype($paramValue))
+            );
+        }
+
+        return $paramValue ?? $defaultValue;
+    }
+
+    public function has(string $param): bool
+    {
+        return $this->offsetExists($param);
     }
 
     private function isValidContent($content)
